5fd38f185440a80f3a5efe01eabce912e3a5a3df
florentina tinte <42583750@fi.unju.edu.ar>
2023-08-02 22:26:25 -0300

    app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app =  express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
-app.listen(3000, ()=> {
+app.listen(3030, ()=> {
     console.log('server')
 });
 app.get('/', (req, res) => {
